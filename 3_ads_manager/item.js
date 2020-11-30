e4f0552560de2cb80cd90a4d7aae14ccf2bc86b0
Subimal Sahu <subimalsahu10@gmail.com>
2020-12-01 01:50:06 +0530

    college project

---
+++ 3_ads_manager/item.js
@@ -0,0 +1,7 @@
+var sum = 0;
+var prices=[23,25,100,400,22];
+for(i=0;i < prices.length; i++){
+prices[i] += 10*(prices[i])/100;
+sum += prices[i];
+}
+console.log(sum);
---
+++ 3_ads_manager/var prices=[23,25,100,400,22].js
@@ -0,0 +1,5 @@
+var prices=[23,25,100,400,22];
+for(i=0;i < prices.length; i++){
+prices[i] += 10*(prices[i])/100;
+sum += prices[i];
+}
